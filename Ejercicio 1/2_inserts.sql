60e8e12af458b1c331658153c93727eaed0a0692
Jaime Gajardo Gangas <135156804+byjmx@users.noreply.github.com>
2024-04-05 17:43:18 -0300

    Add files via upload

---
+++ Ejercicio 1/2_inserts.sql
@@ -0,0 +1,9 @@
+/*Query insert*/
+
+INSERT INTO tabla_a (order_number, header_id) VALUES ('123', '80001');
+INSERT INTO tabla_b (header_id, line_id, cantidad) VALUES ('80001', '10001', '1');
+INSERT INTO tabla_b (header_id, line_id, cantidad) VALUES ('80001', '10002', '3');
+INSERT INTO tabla_b (header_id, line_id, cantidad) VALUES ('80001', '10003', '5');
+INSERT INTO tabla_c (header_id, line_id, hold_price_id, flag) VALUES ('80001', '10001', '2001', 'Y'); 
+INSERT INTO tabla_c (header_id, line_id, hold_price_id, flag) VALUES ('80001', '10002', '2002', 'Y');
+INSERT INTO tabla_c (header_id, line_id, hold_price_id, flag) VALUES ('80001', '10003', '2003', 'N');
